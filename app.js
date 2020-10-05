2647fdf909f860697b84193c4259ff3dba06541e
NotVasil <69978985+NotVasil@users.noreply.github.com>
2020-10-05 15:01:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+files = {
+    "info.txt": "Hello there! I'm Luna and i love programming if you want to see my projects here is my <a href='https://github.com/RealCreepi'>Github</a>"
+}
+$('#crt').terminal({
+    ls: function() {
+        s_return = ""
+        for (file of Object.keys(files)) {
+            s_return += file + "\n"
+        }
+        this.echo(s_return)
+    },
+    cat: function(file) {
+        s_return = files[file]
+        if(s_return) {
+            this.echo(s_return, {raw:true})
+        } else {
+            this.echo(`'${file}' wasn't found!`)
+        }
+    }
+}, {
+    greetings: "Welcome! Use 'ls' to see files and 'cat' to read them."
+});
